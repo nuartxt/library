be8bd3cc087a2d0eb8893a899028bc286b53557c
Anuar <151063105+nuartxt@users.noreply.github.com>
2026-01-31 21:05:00 +0500

    make header, regitster books and cards
    add style and js logic for plus btn

--- js.js
+++ js.js
@@ -1,1 +1,23 @@
+
 console.log("HEllo world!")
+
+let btn = document.querySelector("button");
+let mainBody = document.querySelector("main");
+
+btn.addEventListener("click", () => {
+    //Card div
+    let card = document.createElement("div");
+    card.classList.add("card");
+    mainBody.appendChild(card);
+
+    // //cardbtns
+    let remove = document.createElement("button");
+    remove.classList.add("remove", "cardBtn");
+    remove.textContent = "remove";
+    card.appendChild(remove);
+
+    let read = document.createElement("button");
+    read.classList.add("read", "cardBtn");
+    read.textContent = "read";
+    card.appendChild(read);
+})
