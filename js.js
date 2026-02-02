4e51fade953f07d3ba0b8f5d4468040513280853
Anuar <151063105+nuartxt@users.noreply.github.com>
2026-02-02 18:59:57 +0500

    create add one book to library logic
    add library array and add book on array logic

--- js.js
+++ js.js
@@ -1,10 +1,79 @@
 
-console.log("HEllo world!")
-
-let btn = document.querySelector("button");
 let mainBody = document.querySelector("main");
+let btn_plus = document.querySelector(".btn_plus");
+let confirm = document.querySelector("#confirm");
+let registerBook = document.querySelector(".register");
+let form = document.querySelector("form");
 
-btn.addEventListener("click", () => {
+
+btn_plus.addEventListener("click", () => {
+    registerBook.classList.toggle("hidden");
+});
+
+const myLibrary = [];
+
+
+
+
+function Book(title, author, number, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.number = number;
+    this.read = read;
+
+}
+
+function addBookToLibrary(title, author, number, read) {
+    const book = new Book(title, author, number, read);
+    myLibrary.push(book);
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+
+    const formData = new FormData(form);
+
+    const title = formData.get("titleBook");
+    const author = formData.get("authorBook");
+    const number = formData.get("numberBook");
+    const read = formData.get("readBook");
+
+    addBookToLibrary(title, author, number, read);
+    console.log(myLibrary);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// btn_plus.addEventListener("click", () => {
+//     console.log(title)
+// })
+
+
+
+
+
+/* 
     //Card div
     let card = document.createElement("div");
     card.classList.add("card");
@@ -20,4 +89,4 @@ btn.addEventListener("click", () => {
     read.classList.add("read", "cardBtn");
     read.textContent = "read";
     card.appendChild(read);
-})
+*/
