99f4d8586707a3637bdfd52232df98a063b068b7
Anuar <151063105+nuartxt@users.noreply.github.com>
2026-02-04 16:59:30 +0500

    create add card on display logic
    fix bag with card id

--- js.js
+++ js.js
@@ -1,96 +1,98 @@
-const mainBody = document.querySelector("main");
-const btn_plus = document.querySelector(".btn_plus");
-const confirm = document.querySelector("#confirm");
 const registerBook = document.querySelector(".register");
-const form = document.querySelector("form");
+const btnPlus = document.querySelector(".btn_plus");
+const input = document.querySelectorAll("input");
+const body = document.querySelector("main");
 
-btn_plus.addEventListener("click", () => {
+btnPlus.addEventListener("click", () => {
     registerBook.classList.toggle("hidden");
-});
-
-const myLibrary = [];
-
-
-//our ONE BOOK OBJECT wich we push in array myLibrary
-function Book(title, author, number, read) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.number = number;
-    this.read = read;
-}
-
-//this function push our book obj in array myLibrary
-function addBookToLibrary(title, author, number, read) {
-    const book = new Book(title, author, number, read);
-    myLibrary.push(book);
-}
-
-
-//this part help us create titile, author and etc 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(form);
-
-    const title = formData.get("titleBook");
-    const author = formData.get("authorBook");
-    const number = formData.get("numberBook");
-    const read = formData.get("readBook");
-
-    addBookToLibrary(title, author, number, read);
-    console.log(myLibrary);
-    clearForm(input);
 })
 
 
-const input = document.querySelectorAll("input");
-function clearForm(arr) {
-    arr.forEach(el => {
-        el.value = "";
-    })
-    form.classList.toggle("hidden");
+const library = [];
+
+
+//our ONE BOOK OBJECT wich we push in array myLibrary
+function Book(title, author, bookPages, read, id) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.bookPages = bookPages;
+    this.read = read;
+}
+
+
+//this function push our books obj in array myLibrary
+function adBookToLibrary(title, author, bookPages, read, id) {
+    const book = new Book(title, author, bookPages, read, id);
+    library.push(book);
+    return book;
 }
 
 
 
+//this part help us create titile, author and etc 
+registerBook.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const bookData = new FormData(registerBook);
+
+    const title = bookData.get("titleBook");
+    const author = bookData.get("authorBook");
+    const bookPages = bookData.get("numberBook");
+    const read = bookData.get("readBook");
+    const id = crypto.randomUUID();
+
+    adBookToLibrary(title, author, bookPages, read, id);
+    console.log(library);
+    clearBookRegiater();
+    showBooksOnDisplay(body, title, author, bookPages, read, id);
+})
+
+
+//this function clear bookRegister
+function clearBookRegiater() {
+    input.forEach(el => {
+        el.value = "";
+    })
+    registerBook.classList.toggle("hidden");
+}
 
 
 
+// this function show our books on display
+function showBooksOnDisplay(body, title, author, bookPages, read, id) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-    //Card div
+    //Card info
     let card = document.createElement("div");
     card.classList.add("card");
-    mainBody.appendChild(card);
+    body.appendChild(card);
+
+
+    const cardTitle = document.createElement("h2");
+    const cardAuthor = document.createElement("p");
+    const cardNumber = document.createElement("p");
+    card.appendChild(cardTitle);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardNumber);
+
+    card.id = id;
+    cardTitle.textContent = title;
+    cardAuthor.textContent = author;
+    cardNumber.textContent = `${bookPages} pages`;
+
+
+
 
     // //cardbtns
-    let remove = document.createElement("button");
-    remove.classList.add("remove", "cardBtn");
-    remove.textContent = "remove";
-    card.appendChild(remove);
+    let removeBtn = document.createElement("button");
+    removeBtn.classList.add("remove", "cardBtn");
+    removeBtn.textContent = "remove";
+    card.appendChild(removeBtn);
 
-    let read = document.createElement("button");
-    read.classList.add("read", "cardBtn");
-    read.textContent = "read";
-    card.appendChild(read);
-*/
+    let readBtn = document.createElement("button");
+    readBtn.classList.add("read", "cardBtn");
+    readBtn.textContent = "read";
+    card.appendChild(readBtn);
+
+
+}
